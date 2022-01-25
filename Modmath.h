afc79c6174ea7a8855b014259d4ea32310c338a7
barnden <brandon@nguyen.vc>
2022-01-24 20:05:11 -0600

    Make Modinv and BezoutCoefficients take/return BigInt

--- Modmath.h
+++ Modmath.h
@@ -8,9 +8,9 @@ BigInt gcd(BigInt const& a, BigInt const& b);
 
 BigInt Totient(BigInt const& n);
 
-std::pair<int64_t, int64_t> BezoutCoefficients(int64_t a, int64_t b);
+std::pair<BigInt, BigInt> BezoutCoefficients(BigInt a, BigInt b);
 
-uint64_t Modinv(uint64_t n, uint64_t mod);
+BigInt Modinv(BigInt const& n, BigInt const& mod);
 
 BigInt Modexp(BigInt const& base, BigInt exp, BigInt const& mod);
 
